1f6467bda748cc403afd1ea6a48856ed41671da2
jiiiwonyy <ji__Won_22@naver.com>
2024-06-25 13:13:48 +0900

    chore: 부모 액티비티 설정

--- app/src/main/java/com/example/finalassignment/CustomerCenter.java
+++ app/src/main/java/com/example/finalassignment/CustomerCenter.java
@@ -15,24 +15,4 @@ public class CustomerCenter extends AppCompatActivity {
         setTitle("고객센터");
         setContentView(R.layout.cs);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.mypage_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-        if(id==R.id.home){
-            Intent intent = new Intent(CustomerCenter.this, MainActivity.class);
-            startActivity(intent);
-            showToast("홈으로 메뉴가 클릭되었습니다");
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
 }
--- app/src/main/java/com/example/finalassignment/MyPage.java
+++ app/src/main/java/com/example/finalassignment/MyPage.java
@@ -12,40 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPage extends AppCompatActivity {
 
-    private Button cartButton;
-
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+        setTitle("마이페이지");
 
-        cartButton = findViewById(R.id.cartButton);
-        cartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MyPage.this, Cart.class);
-                startActivity(intent);
-                showToast("장바구니로 이동합니다.");
-            }
-        });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.mypage_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-        if(id==R.id.home){
-            Intent intent = new Intent(MyPage.this, MainActivity.class);
-            startActivity(intent);
-            showToast("홈으로 메뉴가 클릭되었습니다");
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+    
 }
